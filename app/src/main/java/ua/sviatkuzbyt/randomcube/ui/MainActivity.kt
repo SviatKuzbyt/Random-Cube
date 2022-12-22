0384bcf02f2aa81b821d50e20ba7758de1e819ff
Sviat Kuzbyt <sviatoslav.kuzbyt@gmail.com>
2022-12-22 22:55:34 +0200

    I added MainActivity.kt and configed theme

---
+++ app/src/main/java/ua/sviatkuzbyt/randomcube/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package ua.sviatkuzbyt.randomcube.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ua.sviatkuzbyt.randomcube.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
